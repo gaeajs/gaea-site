bb0f42c418660e06b360bf0208aec3a7eec9cf0b
ascoders <576625322@qq.com>
2018-06-16 12:05:10 +0800

    update

---
+++ pri.config.ts
@@ -0,0 +1,7 @@
+import { ProjectConfig } from 'pri/client';
+
+export default {
+  publicPath: '/gaea-site',
+  baseHref: '/gaea-site',
+  staticBuild: true
+} as ProjectConfig;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,8 @@
-import { env } from "pri"
+import Editor from "gaea-editor"
 import * as React from "react"
 
 export default () => (
-  <div>
-    <h1
-      style={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center"
-      }}
-    >
-      Welcome to pri!
-    </h1>
-    <p style={{ padding: "10 50px" }}>
-      Current env: {env.isLocal && "local"}
-      {env.isProd && "prod"}
-    </p>
+  <div style={{ width: "100vw", height: "100vh" }}>
+    <Editor />
   </div>
 )
